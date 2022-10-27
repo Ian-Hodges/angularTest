078d943a0781fa25a05432f23f5fcf0428ef77e8
Ian-Hodges <doctorhodges@gmail.com>
2022-10-27 22:54:13 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+import { AppComponent } from './app.component';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { BookCollectionComponent } from './book-collection/book-collection.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({
+      count: counterReducer,
+      books: booksReducer,
+      collection: collectionReducer,
+    }),
+    HttpClientModule,
+    StoreDevtoolsModule,
+  ],
+  declarations: [
+    AppComponent,
+    MyCounterComponent,
+    BookListComponent,
+    BookCollectionComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
